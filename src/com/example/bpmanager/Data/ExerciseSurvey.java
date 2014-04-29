1b826083bb9a4e2b9d2edf678f17f043c6b08e9e
kyun <lkyunl@naver.com>
2014-04-30 05:15:58 +0900

    1.1

--- src/com/example/bpmanager/Data/ExerciseSurvey.java
+++ src/com/example/bpmanager/Data/ExerciseSurvey.java
@@ -30,7 +30,7 @@ public class ExerciseSurvey extends AbstractSurvey {
 		if(strength < 3) str.append(" 운동 강도 ");
 		//str.append(")");
 
-		if(number > 3 && time > 60 && strength >=2)
+		if(number > 3 && time > 60 && strength >2)
 			return "당신은 운동 시간"+ time+"분, 횟수 "+number+"회, 강도 "+strStrength+"으로, 목표 운동량을 잘 지키고 있습니다.\n 적절한 운동은 혈압을 유지하는데 도움이 됩니다. 지금처럼 목표 운동량을 지키세요.";
 		else return "당신은 운동 시간 "+ time+"분, 횟수 "+number+"회, 강도 "+strStrength+"으로, "+ str.toString() + "(이)가 부족합니다.\n 고혈압 환자의 목표 운동량은 일주일에 4-7회, 한 번에 60분 이상입니다. 운동을 할 때는 보통 이상의 노력이 필요한 강도로 하세요. 고혈압 환자는 적절한 운동만으로 수축기 혈압과 이완기 혈압을 5-7mmHg까지 낮출 수 있습니다. 권고량에 따라 운동을 해보세요.";
 	}
